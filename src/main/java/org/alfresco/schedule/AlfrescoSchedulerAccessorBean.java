8127fe68b7128875f2c3d3c0d48578127006fef3
Alex Mukha <alex.mukha@alfresco.com>
2018-03-01 14:54:24 +0000

    REPO-1295 Refactor trigger injection into schedulers

---
+++ src/main/java/org/alfresco/schedule/AlfrescoSchedulerAccessorBean.java
@@ -0,0 +1,57 @@
+/*
+ * #%L
+ * Alfresco Repository
+ * %%
+ * Copyright (C) 2005 - 2018 Alfresco Software Limited
+ * %%
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package org.alfresco.schedule;
+
+import org.quartz.SchedulerException;
+import org.springframework.scheduling.quartz.SchedulerAccessorBean;
+
+/**
+ * The class is designed to add <code>enabled</code> check to switch on/off the triggers scheduling.
+ * The default is <code>true</code>.
+ */
+public class AlfrescoSchedulerAccessorBean extends SchedulerAccessorBean
+{
+    private boolean enabled = true;
+
+    public boolean isEnabled()
+    {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled)
+    {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws SchedulerException
+    {
+        if (isEnabled())
+        {
+            super.afterPropertiesSet();
+        }
+    }
+}
