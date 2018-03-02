105149668bf1d61a2618e12fc43f90aba376dc58
Alex Mukha <killerboot@users.noreply.github.com>
2018-03-02 14:41:51 +0000

    REPO-1295 Remove unused import

--- src/main/java/org/alfresco/repo/domain/dialect/MySQLClusterNDBDialect.java
+++ src/main/java/org/alfresco/repo/domain/dialect/MySQLClusterNDBDialect.java
@@ -27,7 +27,6 @@ package org.alfresco.repo.domain.dialect;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-//import org.hibernate.dialect.MySQLInnoDBDialect;
 
 /**
  * MySQL Cluster NDB specific DAO overrides
