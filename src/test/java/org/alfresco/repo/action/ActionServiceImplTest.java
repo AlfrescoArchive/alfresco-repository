31dd12c418a53ddb133390fe06969045761a7784
Stefan Kopf <stefan.kopf@alfresco.com>
2019-01-20 21:56:12 +0100

    Fix ActionService test case

--- src/test/java/org/alfresco/repo/action/ActionServiceImplTest.java
+++ src/test/java/org/alfresco/repo/action/ActionServiceImplTest.java
@@ -811,36 +811,39 @@ public class ActionServiceImplTest extends BaseAlfrescoSpringTest
     @Test
     public void testActionResult()
     {
-        // Create the script node reference
-        NodeRef script = this.nodeService.createNode(
-                this.folder,
-                ContentModel.ASSOC_CONTAINS,
-                QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "testScript.js"),
-                ContentModel.TYPE_CONTENT).getChildRef();
-        this.nodeService.setProperty(script, ContentModel.PROP_NAME, "testScript.js");
-        ContentWriter contentWriter = this.contentService.getWriter(script, ContentModel.PROP_CONTENT, true);
-        contentWriter.setMimetype("text/plain");
-        contentWriter.setEncoding("UTF-8");
-        contentWriter.putContent("\"VALUE\";");
-        
-        // Create the action
-        Action action1 = this.actionService.createAction(ScriptActionExecuter.NAME);
-        action1.setParameterValue(ScriptActionExecuter.PARAM_SCRIPTREF, script);
-        
-        // Execute the action as Administrator. The ScriptAction has to run as a full user (instead of as System) so
-        // that we can setup the Person object in the ScriptNode
-        AuthenticationUtil.runAs(new AuthenticationUtil.RunAsWork<Void>( ) {
-            @Override
-            public Void doWork() throws Exception {
-                ActionServiceImplTest.this.actionService.executeAction(action1, ActionServiceImplTest.this.nodeRef);
-                return null;
-            }
-        }, AuthenticationUtil.getAdminUserName());
+        // We need to run this test as Administrator. The ScriptAction has to run as a full user (instead of as System)
+        // so that we can setup the Person object in the ScriptNode
+        AuthenticationUtil.setFullyAuthenticatedUser(AuthenticationUtil.getAdminUserName());
+        try
+        {
+            // Create the script node reference
+            NodeRef script = this.nodeService.createNode(
+                    this.folder,
+                    ContentModel.ASSOC_CONTAINS,
+                    QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "testScript.js"),
+                    ContentModel.TYPE_CONTENT).getChildRef();
+            this.nodeService.setProperty(script, ContentModel.PROP_NAME, "testScript.js");
+            ContentWriter contentWriter = this.contentService.getWriter(script, ContentModel.PROP_CONTENT, true);
+            contentWriter.setMimetype("text/plain");
+            contentWriter.setEncoding("UTF-8");
+            contentWriter.putContent("\"VALUE\";");
 
-        // Get the result
-        String result = (String)action1.getParameterValue(ActionExecuter.PARAM_RESULT);
-        assertNotNull(result);
-        assertEquals("VALUE", result);                
+            // Create the action
+            Action action1 = this.actionService.createAction(ScriptActionExecuter.NAME);
+            action1.setParameterValue(ScriptActionExecuter.PARAM_SCRIPTREF, script);
+
+            // Execute the action
+            this.actionService.executeAction(action1, this.nodeRef);
+
+            // Get the result
+            String result = (String)action1.getParameterValue(ActionExecuter.PARAM_RESULT);
+            assertNotNull(result);
+            assertEquals("VALUE", result);
+        }
+        finally
+        {
+            AuthenticationUtil.clearCurrentSecurityContext();
+        }
     }
     
     /** ===================================================================================
