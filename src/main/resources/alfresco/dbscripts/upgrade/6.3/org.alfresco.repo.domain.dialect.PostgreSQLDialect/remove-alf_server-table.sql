363b78a263c18a9a0393a11b61e2898c337d8af4
David Edwards <david.edwards@alfresco.com>
2019-07-30 16:32:36 +0100

    REPO-4547 MySQL update script updated

--- src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.PostgreSQLDialect/remove-alf_server-table.sql
+++ src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.PostgreSQLDialect/remove-alf_server-table.sql
@@ -3,13 +3,11 @@
 -- Database:   PostgreSQL
 -- Since:      V6.3
 -- Author:     David Edwards
--- Author:     Alex Mukha
 --
 -- Please contact support@alfresco.com if you need assistance with the upgrade.
 --
 
 -- ASSIGN:SYSTEM_NS_ID=id
--- TODO: Find out what below line is for and how to update it for current schema (was in NodeDeleted.sql which is being used as a template) and why they are used
 SELECT id FROM alf_namespace WHERE uri = 'http://www.alfresco.org/model/system/1.0';
 
 -- ASSIGN:NEXT_ID=id
@@ -22,19 +20,44 @@ INSERT INTO alf_qname (id, version, ns_id, local_name) VALUES (${NEXT_ID}, 0, ${
 -- ASSIGN:DELETED_TYPE_ID=id
 SELECT id FROM alf_qname WHERE ns_id = ${SYSTEM_NS_ID} AND local_name = 'deleted';
 
--- TODO DROP the indexes
+-- DROP the indexes
+DROP INDEX fk_alf_txn_svr;
+DROP INDEX idx_alf_txn_ctms;
 
--- TODO DROP the constraints
+-- DROP the constraints alf_transaction
+ALTER TABLE alf_transaction DROP CONSTRAINT fk_alf_txn_svr;
 
--- TODO Rename existing alf_transaction to t_alf_transaction
+-- Rename existing alf_transaction to t_alf_transaction
+ALTER TABLE alf_transaction RENAME TO t_alf_transaction;
 
--- TODO Create new alf_transaction table with new schema
+-- Create new alf_transaction table with new schema
+CREATE TABLE alf_transaction
+(
+    id INT8 NOT NULL,
+    version INT8 NOT NULL,
+    change_txn_id VARCHAR(56) NOT NULL,
+    commit_time_ms INT8,
+    PRIMARY KEY (id)
+);
+CREATE INDEX idx_alf_txn_ctms ON alf_transaction (commit_time_ms, id);
 
--- TODO Migrate the data from t_alf_transaction to the new alf_transaction
+--  Migrate the data from t_alf_transaction to the new alf_transaction
+--FOREACH t_alf_node.id system.upgrade.alf_node_deleted_type.batchsize
+INSERT INTO alf_transaction
+(id, version, change_txn_id, commit_time_ms)
+(
+    SELECT
+       id, version, (CASE WHEN node_deleted THEN ${DELETED_TYPE_ID} ELSE server_id END), change_txn_id, commit_time_ms
+    FROM
+       t_alf_transaction
+    where
+       id >= ${LOWERBOUND} AND id <= ${UPPERBOUND}
+);
+-- DROP old table t_alf_transaction
+DROP TABLE t_alf_transaction;
 
--- TODO DROP old table
-
--- TODO rebuilt indexes and constraints on new table
+-- DROP alf_server table
+DROP TABLE alf_server;
 
 --
 -- Record script finish
