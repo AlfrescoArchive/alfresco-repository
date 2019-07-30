2b1f33a02c92e9ebf726520277b6659363497e4e
David Edwards <david.edwards@alfresco.com>
2019-07-30 15:51:42 +0100

    REPO-4547 Patch files created for MySQL&PostgreSQL

---
+++ src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.MySQLinnoDBDialect/remove-alf_server-table-and-foregin-keys.sql
@@ -0,0 +1,9 @@
+--
+-- Title:      Remove alf_server table and indexes
+-- Database:   MySQL
+-- Since:      V6.3
+-- Author:     David Edwards
+--
+-- Please contact support@alfresco.com if you need assistance with the upgrade.
+--
+
