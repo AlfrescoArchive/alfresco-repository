148f13b9435586edf09e574f153619a97c4a3675
Alex Mukha <alex.mukha@alfresco.com>
2019-07-30 16:17:43 +0100

    Add spring bean for patches

--- src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.MySQLinnoDBDialect/remove-alf_server-table.sql
+++ src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.MySQLinnoDBDialect/remove-alf_server-table.sql
@@ -1,9 +1,21 @@
 --
--- Title:      Remove alf_server table and indexes
+-- Title:      Remove alf_server table
 -- Database:   MySQL
 -- Since:      V6.3
 -- Author:     David Edwards
+-- Author:     Alex Mukha
 --
 -- Please contact support@alfresco.com if you need assistance with the upgrade.
 --
 
+--
+-- Record script finish
+--
+DELETE FROM alf_applied_patch WHERE id = 'patch.db-V6.3-remove-alf_server-table';
+INSERT INTO alf_applied_patch
+  (id, description, fixes_from_schema, fixes_to_schema, applied_to_schema, target_schema, applied_on_date, applied_to_server, was_executed, succeeded, report)
+  VALUES
+  (
+    'patch.db-V6.3-remove-alf_server-table', 'Remove alf_server table',
+    0, 13000, -1, 13001, null, 'UNKNOWN', ${TRUE}, ${TRUE}, 'Script completed'
+  );
--- src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.PostgreSQLDialect/remove-alf_server-table.sql
+++ src/main/resources/alfresco/dbscripts/upgrade/6.3/org.alfresco.repo.domain.dialect.PostgreSQLDialect/remove-alf_server-table.sql
@@ -3,6 +3,7 @@
 -- Database:   PostgreSQL
 -- Since:      V6.3
 -- Author:     David Edwards
+-- Author:     Alex Mukha
 --
 -- Please contact support@alfresco.com if you need assistance with the upgrade.
 --
@@ -37,12 +38,12 @@ SELECT id FROM alf_qname WHERE ns_id = ${SYSTEM_NS_ID} AND local_name = 'deleted
 
 --
 -- Record script finish
--- 
-DELETE FROM alf_applied_patch WHERE id = 'patch.db-V6.3-remove-alf_server';
+--
+DELETE FROM alf_applied_patch WHERE id = 'patch.db-V6.3-remove-alf_server-table';
 INSERT INTO alf_applied_patch
   (id, description, fixes_from_schema, fixes_to_schema, applied_to_schema, target_schema, applied_on_date, applied_to_server, was_executed, succeeded, report)
   VALUES
   (
-    'patch.db-V4.1-NodeDeleted', 'Manually executed script upgrade V4.1: Remove node_deleted',
-    0, 6014, -1, 6015, null, 'UNKNOWN', ${TRUE}, ${TRUE}, 'Script completed'
+    'patch.db-V6.3-remove-alf_server-table', 'Remove alf_server table',
+    0, 13000, -1, 13001, null, 'UNKNOWN', ${TRUE}, ${TRUE}, 'Script completed'
   );
