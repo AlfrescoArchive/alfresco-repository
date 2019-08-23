5f1bd028c9b3c9f01ce53b541688aba4d4baf80c
Ancuta Morarasu <ancuta.morarasu@ness.com>
2019-08-23 13:07:13 +0300

    REPO-4612 / MNT-20833: Forgot import

--- src/test/java/org/alfresco/repo/jscript/ScriptNodeTest.java
+++ src/test/java/org/alfresco/repo/jscript/ScriptNodeTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Alfresco Repository
  * %%
- * Copyright (C) 2005 - 2017 Alfresco Software Limited
+ * Copyright (C) 2005 - 2019 Alfresco Software Limited
  * %%
  * This file is part of the Alfresco software. 
  * If the software was purchased under a paid Alfresco license, the terms of 
@@ -28,6 +28,7 @@ package org.alfresco.repo.jscript;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
