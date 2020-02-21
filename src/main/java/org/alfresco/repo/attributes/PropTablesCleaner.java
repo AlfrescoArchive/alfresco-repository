e453d7d6417086583071ad516c2454d6c9303378
Cristian Turlica <cristian.turlica@ness.com>
2020-02-21 11:22:35 +0200

    MNT-21083: Alternative clean up script    Cherry picked commit fea4ad33665f1e17aca499ad51c275534e668d6a

--- src/main/java/org/alfresco/repo/attributes/PropTablesCleaner.java
+++ src/main/java/org/alfresco/repo/attributes/PropTablesCleaner.java
@@ -4,21 +4,21 @@
  * %%
  * Copyright (C) 2005 - 2016 Alfresco Software Limited
  * %%
- * This file is part of the Alfresco software. 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * This file is part of the Alfresco software.
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -28,11 +28,14 @@ package org.alfresco.repo.attributes;
 import org.alfresco.repo.domain.propval.PropertyValueDAO;
 import org.alfresco.repo.lock.JobLockService;
 import org.alfresco.repo.lock.LockAcquisitionException;
+import org.alfresco.repo.security.sync.ChainingUserRegistrySynchronizer;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.util.PropertyCheck;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.util.Properties;
 
 /**
  * Cleaner of unused values from the alf_prop_xxx tables.
@@ -41,8 +44,12 @@ import org.apache.commons.logging.LogFactory;
  */
 public class PropTablesCleaner
 {
+    private static final String PROPERTY_PROP_TABLE_CLEANER_ALG = "system.prop_table_cleaner.algorithm";
+    private static final String PROP_TABLE_CLEANER_ALG_V2 = "V2";
+
     private PropertyValueDAO propertyValueDAO;
     private JobLockService jobLockService;
+    private Properties globalProperties;
 
     /* 1 minute */
     private static final long LOCK_TTL = 360000L;
@@ -60,10 +67,16 @@ public class PropTablesCleaner
         this.jobLockService = jobLockService;
     }
 
+    public void setGlobalProperties(Properties globalProperties)
+    {
+        this.globalProperties = globalProperties;
+    }
+
     public void checkProperties()
     {
         PropertyCheck.mandatory(this, "jobLockService", jobLockService);
         PropertyCheck.mandatory(this, "propertyValueDAO", propertyValueDAO);
+        PropertyCheck.mandatory(this, "globalProperties", globalProperties);
     }
 
     /**
@@ -73,13 +86,24 @@ public class PropTablesCleaner
     public void execute()
     {
         checkProperties();
-        
-        String lockToken = null;
+
+        String propCleanUplockToken = null;
+        String ldapSyncLockTocken = null;
         try
         {
-            // Get a lock
-            lockToken = jobLockService.getLock(LOCK_QNAME, LOCK_TTL);
-            propertyValueDAO.cleanupUnusedValues();
+            // Get a lock for cleanup
+            propCleanUplockToken = jobLockService.getLock(LOCK_QNAME, LOCK_TTL);
+            // Get a lock for LDAP sync as well, see REPO-4556
+            ldapSyncLockTocken = jobLockService.getLock(ChainingUserRegistrySynchronizer.LOCK_QNAME, LOCK_TTL);
+
+            if (PROP_TABLE_CLEANER_ALG_V2.equalsIgnoreCase(getAlgorithm()))
+            {
+                propertyValueDAO.cleanupUnusedValuesV2();
+            }
+            else
+            {
+                propertyValueDAO.cleanupUnusedValues();
+            }
         }
         catch (LockAcquisitionException e)
         {
@@ -90,11 +114,22 @@ public class PropTablesCleaner
         }
         finally
         {
-            if (lockToken != null)
+            if (propCleanUplockToken != null)
             {
                 try
                 {
-                    jobLockService.releaseLock(lockToken, LOCK_QNAME);
+                    jobLockService.releaseLock(propCleanUplockToken, LOCK_QNAME);
+                }
+                catch (LockAcquisitionException e)
+                {
+                    // Ignore
+                }
+            }
+            if (ldapSyncLockTocken != null)
+            {
+                try
+                {
+                    jobLockService.releaseLock(ldapSyncLockTocken, ChainingUserRegistrySynchronizer.LOCK_QNAME);
                 }
                 catch (LockAcquisitionException e)
                 {
@@ -102,5 +137,10 @@ public class PropTablesCleaner
                 }
             }
         }
+    }
+
+    private String getAlgorithm()
+    {
+        return globalProperties.getProperty(PROPERTY_PROP_TABLE_CLEANER_ALG);
     }
 }
