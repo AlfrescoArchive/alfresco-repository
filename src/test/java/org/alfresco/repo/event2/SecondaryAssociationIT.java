1d5764571a7bcc27a92848ebb34e6e98339ce5f7
aftene <iulian.aftene@ness.com>
2020-05-28 12:59:40 +0300

    REPO-5179 - Create node association Integration tests - add SecondaryAssociationIT class

---
+++ src/test/java/org/alfresco/repo/event2/SecondaryAssociationIT.java
@@ -0,0 +1,21 @@
+package org.alfresco.repo.event2;
+
+import org.junit.Test;
+
+/**
+ * @author Iulian Aftene
+ */
+public class SecondaryAssociationIT extends AbstractContextAwareRepoEvent
+{
+    @Test
+    public void testCreateAssocPeerEvent()
+    {
+
+    }
+
+    @Test
+    public void testDeleteAssocPeerEvent()
+    {
+
+    }
+}
