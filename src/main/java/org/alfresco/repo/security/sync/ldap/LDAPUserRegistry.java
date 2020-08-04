2a82d817c8c34f78adf3dfe4ab88830f0526fccf
Abdul Mohammed <abdul.mohammed@alfresco.com>
2020-08-04 17:23:30 +0100

    Catch reported exceptions to filter ldap sync aborts

--- src/main/java/org/alfresco/repo/security/sync/ldap/LDAPUserRegistry.java
+++ src/main/java/org/alfresco/repo/security/sync/ldap/LDAPUserRegistry.java
@@ -54,6 +54,7 @@ import javax.naming.InvalidNameException;
 import javax.naming.Name;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.ServiceUnavailableException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
@@ -881,6 +882,26 @@ public class LDAPUserRegistry implements UserRegistry, LDAPNameResolver, Initial
                                             childAssocs.add("GROUP_" + nameAttribute.get());
                                             continue;
                                         }
+                                    }
+                                    catch (ServiceUnavailableException e)
+                                    {
+                                        // MNT-21614: Check & fail if communication breaks due to javax.naming.ServiceUnavailableException
+                                        if (e.getMessage() != null)
+                                        {
+                                            Object[] params = {e.getLocalizedMessage() };
+                                            throw new AlfrescoRuntimeException("synchronization.err.ldap.search", params, e);
+                                        }
+                                        continue;
+                                    }
+                                    catch (CommunicationException e)
+                                    {
+                                        // MNT-21614: Check & fail if communication breaks due to javax.naming.CommunicationException
+                                        if (e.getMessage() != null)
+                                        {
+                                            Object[] params = {e.getLocalizedMessage() };
+                                            throw new AlfrescoRuntimeException("synchronization.err.ldap.search", params, e);
+                                        }
+                                        continue;
                                     }
                                     catch (NamingException e)
                                     {
